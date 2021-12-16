27974c568dc38ee9b71543d146f53a28a9fa5ed1
sellersd <32783481+sellersd@users.noreply.github.com>
2021-12-15 19:06:48 -0600

    Add files via upload

---
+++ APCSA_Final.java
@@ -0,0 +1,110 @@
+/********************
+ * Name
+ * AP CS A Final
+ * Fall 2021
+ *******************/
+
+/********************
+ * Using the following starting code,
+ * create a program that will allow
+ * the creation of employees and
+ * their storage in a single array.
+ * There can be a generic employee
+ * type a business may use to add
+ * an uncharacterized employee to 
+ * their log. There are also employee
+ * types of software developer and 
+ * network technician.
+ *
+ * Sample data
+ * <name> <employment type> <salary> <DOB>
+ * Abigail Alexander Employee $50,000 05/01/1978
+ * <name> <employment type> <salary> <DOB> <languages>
+ * Brandon Bronson Developer $125,000 03/07/1985 Java
+ * <name> <employment type> <salary> <DOB> <certifications>
+ * Claire Carlson Technician $45,000 01/13/1992 Security+
+ *
+ *******************/
+
+
+class APCSA_Final {
+	public static void main(String[] args) {
+
+	// For a C,
+	// create an array to hold all three types
+	
+	
+	// For an A, 
+	// print out all of the objects in the array
+	// using there toString() method
+
+	}
+}
+
+class Employee {
+	// For a C,
+	// create instance variables to hold data 
+	// from the list of employees
+	
+	// For a C,
+	// create a default constructor
+	
+	// For a B,
+	// create an overloaded constructor
+	// this constructor should accept
+	// arguments so that the object
+	// is created with the values passed
+	// in from the user
+	
+	// For an A,
+	// create a toString() method that
+	// will return a string of the
+	// information  about the object 
+	// Format is unimportant here.
+}
+
+class Developer extends Employee {
+	// For a C,
+	// create instance variables to hold data 
+	// from the list of employees
+
+	
+	// For a C,
+	// create a default constructor
+	
+	// For a B,
+	// create an overloaded constructor
+	// this constructor should accept
+	// arguments so that the object
+	// is created with the values passed
+	// in from the user
+	
+	// For an A,
+	// create a toString() method that
+	// will return a string of the
+	// information  about the object 
+	// Format is unimportant here.
+}
+
+class Technician extends Employee {
+	// For a C,
+	// create instance variables to hold data 
+	// from the list of employees
+
+	
+	// For a C,
+	// create a default constructor
+	
+	// For a B,
+	// create an overloaded constructor
+	// this constructor should accept
+	// arguments so that the object
+	// is created with the values passed
+	// in from the user
+	
+	// For an A,
+	// create a toString() method that
+	// will return a string of the
+	// information  about the object 
+	// Format is unimportant here.
+}

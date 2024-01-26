651993a2e7e78a4ccd709b8e9752ef3e75dcac78
sellersd <32783481+sellersd@users.noreply.github.com>
2024-01-26 11:48:47 -0600

    Create Peano.java
    Starter code added

---
+++ Assignments/Methods/Peano.java
@@ -0,0 +1,17 @@
+/* Name
+* 2024JAN26
+* Peano
+* Given the natural numbers there is always 
+* a next largest natural number. We can find
+* the successive natural number by adding one. 
+* This program shall consists of methods that take
+* two arbitrary natural numbers and use the
+* successor function perform addition, 
+* multiplication, and exponentiation.
+*/
+
+class Peano {
+	public static void main(String[] args) {
+		
+	}
+}

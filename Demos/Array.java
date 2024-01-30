acb091edc77ed2f71c0d2a8ed4e509d06d27462e
david <david.m.sellers@gmail.com>
2024-01-30 11:23:42 -0600

    updated assignment

--- Demos/Array.java
+++ Demos/Array.java
@@ -13,14 +13,24 @@ class Array {
     int[] nums;
 
    // An array once declared must be initialized, this allocates
-   // the memory needed.
+   // the memory needed. This array allocates space for 20 ints.
    nums = new int[20];
 
+   // This can all be accomplished in a a single statement.
+   int[] nums2 = new int[20];
+
    // Once you have declared  and initialized the array you
-    // can place values in the array
+   // can place values in the array
     nums[0] = 14;
     System.out.println(nums[0]);
 
+   // Looping can be an efficient way to initialize the array.
+   for(int i = 0; i < nums.length(); i++) {
+      nums[i] = i;
+   }
 
+   // Arrays are indexed, each location in the array has a specific
+   // index that refers to it. When we say new int[n] the array
+   // has n locations indexed by the integers 0 through n-1
   }
 }
